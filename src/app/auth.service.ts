74dd70dbfa42bf41092b9a21553bfaeb40ce4e67
bastian bastias <bastian.bastias@gmail.com>
2022-12-03 20:50:28 -0500

    login logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -24,6 +24,7 @@ export class AuthService {
   }
 
   logout() {
+    console.log("logout: AuthService");
     localStorage.removeItem("access_token");
   }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Event, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  loginStatus: any;
+  constructor(private auth: AuthService, private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.loginStatus = this.auth.isAunthenticated();
+        console.log("loginSTatus: "+this.loginStatus);        
+      }
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    console.log("logout");
+    this.auth.logout();
+    this.router.navigate(["/"]);  
   }
 
 }
