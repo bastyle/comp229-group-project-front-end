405839010204e621190d781356e74658dcf5e748
bastian bastias <bastian.bastias@gmail.com>
2022-12-03 21:11:25 -0500

    login required

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { PostsDetailsComponent } from './posts-details/posts-details.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { PostsComponent } from './posts/posts.component';
+import { GuardAuthServiceService } from './guard-auth-service.service';
 
 
 const routes: Routes = [
-  { path: 'post/:postId', component: PostsDetailsComponent},
-  { path: 'post', component:AddComponent},
+  { path: 'post/:postId', component: PostsDetailsComponent, canActivate: [GuardAuthServiceService]},
+  { path: 'post', component:AddComponent, canActivate: [GuardAuthServiceService]},
   { path: 'register_user', component: UserRegisterComponent},
-  { path: 'posts', component:PostsComponent},
+  { path: 'posts', component:PostsComponent, canActivate: [GuardAuthServiceService]},
   //{ path: 'post/form', component: PostsDetailsComponent}
   { path: "login", component: LoginComponent }
 ];
